2ccf653c999650c530164d690bd751c95218544d
Rashmi <rashmiap.10@gmail.com>
2018-09-11 13:14:02 +0530

    styled component added testing initial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
 import './App.css';
+
+const AppWrapper = styled.div`
+  background-color: #222;
+  height: 12rem;
+  padding: 1rem;
+  color: white;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <AppWrapper>
         hello world!
-      </div>
+      </AppWrapper>
     );
   }
 }
