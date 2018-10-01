26c613e6048ec66ffb3146ed3bcbdd98c7ce3173
Rashmi <rashmiap.10@gmail.com>
2018-10-01 12:14:14 +0530

    Style updated Rules component added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import Rules from './Components/Rules';
 import './App.css';
 
 const AppWrapper = styled.div`
   background-color: #222;
-  height: 12rem;
   padding: 1rem;
   color: white;
+  text-align: center;
 `
 
 class App extends Component {
   render() {
     return (
       <AppWrapper>
-        hello world!
+        <h2>Bulls and Cows game</h2>
+        <Rules />
       </AppWrapper>
     );
   }
