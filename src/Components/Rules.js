050755e7cdb98dc5d7f5b53c25d848682e0e7bcd
Rashmi <rashmiap.10@gmail.com>
2018-10-01 12:13:49 +0530

    Rules component added with initial rules text

---
+++ src/Components/Rules.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const RulesWrapper = styled.div`
+  padding: 1rem;
+  max-width: 40%;
+  margin: 0 auto;
+  text-align: left;
+  border-radius: 20px;
+  border: 10px solid #f0f8fc;
+`
+const RulesLink = styled.a`
+  font-size: 14px;
+  font-weight: normal;
+  color: white;
+  padding: 1em;
+  display: block;
+`
+
+class Rules extends Component {
+  constructor(){
+    super();
+    this.state = {
+      scrollOpen: false,
+    }
+  }
+  __handleRulesView(){
+    this.setState(prevState => {
+      return {
+        scrollOpen: !prevState.scrollOpen,
+      }
+    })
+  }
+  render() {
+    const { scrollOpen } = this.state;
+    return (
+      <div>
+        <RulesLink href="javascript:void(0)" onClick={this.__handleRulesView.bind(this)}>{ !scrollOpen ? `HOW TO PLAY` : `LET'S PLAY` }</RulesLink>
+        {
+          scrollOpen ?
+          <RulesWrapper>
+            <p>The goal of this game is to guess a 4-digit number within the least number of attempts possible.</p>
+            <p>With every guess, you will get a feedback indicating how many A and B you got with the guess.</p>
+            <p>An A means: one of the digits is correct, and is also at the right place.</p>
+            <p>A B means: one of the digits is a right number, but not at the right place.</p>
+            <p> For example, given that the answer is 0130, a guess of 3610 will receive 1A2B. 1A is 0, 2B are 1 and 3.</p>
+          </RulesWrapper>: ''
+
+        }
+
+      </div>
+    );
+  }
+}
+
+export default Rules;
